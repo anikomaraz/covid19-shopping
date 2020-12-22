65428b60f5198fd2be00bd80d1ccf1ba2a86ee9a
EvaKatzinger <74974643+EvaKatzinger@users.noreply.github.com>
2020-12-22 17:02:51 +0100

    I have calculated H2

--- Code/BAs/statistics_BAs.R
+++ Code/BAs/statistics_BAs.R
@@ -48,6 +48,59 @@ BAs <- c("BAs_shopping", "BAs_alcohol", "BAs_smoking",
 # calculate a correlation coefficient (Spearman) between each behavioural problem and PSS, the total score of the past-7-day distress scale
 # see this guide for choosing a corr.coeff.: https://journals.sagepub.com/doi/pdf/10.1177/8756479308317006
 #(Eva)
+library(car)
+#creat data.frame with relevant variables
+dataframeH2<- data.frame(as.numeric(data_shoppingCovid19$BAs_shopping), as.numeric(data_shoppingCovid19$BAs_alcohol), 
+                                    as.numeric(data_shoppingCovid19$BAs_smoking), as.numeric(data_shoppingCovid19$BAs_legal_drug),
+                                    as.numeric(data_shoppingCovid19$BAs_illegal_drug), as.numeric(data_shoppingCovid19$BAs_gambling),
+                                    as.numeric(data_shoppingCovid19$BAs_gaming), as.numeric(data_shoppingCovid19$BAs_overeating), data_shoppingCovid19$PSS)
+#rename colums
+colnames(dataframeH2)
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_shopping."] <- "BAs_shopping"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_alcohol."] <- "BAs_alcohol"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_smoking."] <- "BAs_smoking"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_legal_drug."] <- "BAs_legal_drug"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_illegal_drug."] <- "BAs_illegal_drug"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_gambling."] <- "BAs_gambling"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_gaming."] <- "BAs_gaming"
+names(dataframeH2)[names(dataframeH2) == "as.numeric.data_shoppingCovid19.BAs_overeating."] <- "BAs_overeating"
+names(dataframeH2)[names(dataframeH2) == "data_shoppingCovid19.PSS"] <- "PSS"
+
+#Tranformation: categorial answers into numeric 
+dataframeH2$BAs_shopping <- recode(var=dataframeH2$BAs_shopping,
+                                    recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_alcohol <- recode(var=dataframeH2$BAs_alcohol,
+                                   recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_smoking <- recode(var=dataframeH2$BAs_smoking,
+                                   recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_legal_drug <- recode(var=dataframeH2$BAs_legal_drug,
+                                  recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_illegal_drug <- recode(var=dataframeH2$BAs_illegal_drug,
+                                   recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_gambling <- recode(var=dataframeH2$BAs_gambling,
+                                  recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_gaming <- recode(var=dataframeH2$BAs_gaming,
+                                  recodes="'not at all'= 1; 'a little bit'= 2; 
+                                   'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+dataframeH2$BAs_overeating <- recode(var=dataframeH2$BAs_overeating, 
+                                     recodes = "'not at all'= 1; 'a little bit'= 2;
+                                     'somewhat'= 3; 'quite a lot'= 4; 'too much' = 5")
+
+#Correlations between each behavioural problem and PSS
+cor.test(dataframeH2$BAs_shopping, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_alcohol, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_smoking, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_legal_drug, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_illegal_drug, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_gambling, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_gaming, dataframeH2$PSS, method=c("spearman"), na.rm = T)
+cor.test(dataframeH2$BAs_overeating, dataframeH2$PSS, method=c("spearman"), na.rm = T)
 
 
 #######################################################
