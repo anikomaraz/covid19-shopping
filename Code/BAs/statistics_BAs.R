e8805a48d6514465932c53a457cf57851e27a548
EvaKatzinger <74974643+EvaKatzinger@users.noreply.github.com>
2020-12-30 15:45:14 +0100

    finished H1

--- Code/BAs/statistics_BAs.R
+++ Code/BAs/statistics_BAs.R
@@ -35,7 +35,181 @@ BAs <- c("BAs_shopping", "BAs_alcohol", "BAs_smoking",
          "BAs_gaming", "BAs_overeating")
 
 # test the linearity among correlation coefficients calculated at each point of time
-# (Aniko)
+# (Eva)
+plotcorrdisandBA<- data.frame(as.numeric(data_shoppingCovid19$BAs_shopping), as.numeric(data_shoppingCovid19$BAs_alcohol), 
+                              as.numeric(data_shoppingCovid19$BAs_smoking), as.numeric(data_shoppingCovid19$BAs_legal_drug),
+                              as.numeric(data_shoppingCovid19$BAs_illegal_drug), as.numeric(data_shoppingCovid19$BAs_gambling),
+                              as.numeric(data_shoppingCovid19$BAs_gaming), as.numeric(data_shoppingCovid19$BAs_overeating), 
+                              as.numeric(data_shoppingCovid19$time_batch), data_shoppingCovid19$PSS, data_shoppingCovid19$stress_outbreak)
+
+#rename colums
+colnames(plotcorrdisandBA)
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_shopping."] <- "BAs_shopping"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_alcohol."] <- "BAs_alcohol"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_smoking."] <- "BAs_smoking"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_legal_drug."] <- "BAs_legal_drug"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_illegal_drug."] <- "BAs_illegal_drug"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_gambling."] <- "BAs_gambling"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_gaming."] <- "BAs_gaming"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.BAs_overeating."] <- "BAs_overeating"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "as.numeric.data_shoppingCovid19.time_batch."] <- "time_batch"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "data_shoppingCovid19.PSS"] <- "PSS"
+names(plotcorrdisandBA)[names(plotcorrdisandBA) == "data_shoppingCovid19.stress_outbreak"] <- "stress_outbreak"
+head(plotcorrdisandBA)
+
+#calculate correlation between BAs and PSS 
+#shopping
+require(plyr)
+funcshopping <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORshopping = cor(plotcorrdisandBA$BAs_shopping, plotcorrdisandBA$PSS )))
+}
+ashopping <-ddply(plotcorrdisandBA, .(time_batch), funcshopping)
+
+#alcohol
+require(plyr)
+funcalcohol <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORalcohol = cor(plotcorrdisandBA$BAs_alcohol, plotcorrdisandBA$PSS )))
+}
+aalcohol <-ddply(plotcorrdisandBA, .(time_batch), funcalcohol)
+
+#smoking
+require(plyr)
+funcsmoking <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORsmoking = cor(plotcorrdisandBA$BAs_smoking, plotcorrdisandBA$PSS )))
+}
+asmoking <-ddply(plotcorrdisandBA, .(time_batch), funcsmoking)
+
+#legal drug
+require(plyr)
+funclegaldrug <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORlegaldrug = cor(plotcorrdisandBA$BAs_legal_drug, plotcorrdisandBA$PSS )))
+}
+alegaldrug <-ddply(plotcorrdisandBA, .(time_batch), funclegaldrug)
+
+#illegal drug
+require(plyr)
+funcillegaldrug <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORillegaldrug = cor(plotcorrdisandBA$BAs_illegal_drug, plotcorrdisandBA$PSS )))
+}
+aillegaldrug <-ddply(plotcorrdisandBA, .(time_batch), funcillegaldrug)
+
+#gambling
+require(plyr)
+funcgambling <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORgambling = cor(plotcorrdisandBA$BAs_gambling, plotcorrdisandBA$PSS )))
+}
+agambling <-ddply(plotcorrdisandBA, .(time_batch), funcgambling)
+
+#gaming
+require(plyr)
+funcgaming <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORgaming = cor(plotcorrdisandBA$BAs_gaming, plotcorrdisandBA$PSS )))
+}
+agaming <-ddply(plotcorrdisandBA, .(time_batch), funcgaming)
+
+#overeating
+require(plyr)
+funcovereating <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORovereating = cor(plotcorrdisandBA$BAs_overeating, plotcorrdisandBA$PSS )))
+}
+aovereating <-ddply(plotcorrdisandBA, .(time_batch), funcovereating)
+
+#make a dataframe for H1: Correlation BAs and PSS
+H1BAsPSS <- data.frame(ashopping, aalcohol$CORalcohol, asmoking$CORsmoking, alegaldrug$CORlegaldrug, aillegaldrug$CORillegaldrug , agambling$CORgambling, agaming$CORgaming, aovereating$CORovereating)
+colnames(H1BAsPSS) #tidying the colnames
+names(H1BAsPSS)[names(H1BAsPSS) == "aalcohol.CORalcohol"] <- "CORalcohol"
+names(H1BAsPSS)[names(H1BAsPSS) == "asmoking.CORsmoking"] <- "CORsmoking"
+names(H1BAsPSS)[names(H1BAsPSS) == "alegaldrug.CORlegaldrug"] <- "CORlegaldrug"
+names(H1BAsPSS)[names(H1BAsPSS) == "aillegaldrug.CORillegaldrug"] <- "CORillegaldrug"
+names(H1BAsPSS)[names(H1BAsPSS) == "agambling.CORgambling"] <- "CORgambling"
+names(H1BAsPSS)[names(H1BAsPSS) == "agaming.CORgaming"] <- "CORgaming"
+names(H1BAsPSS)[names(H1BAsPSS) == "aovereating.CORovereating"] <- "CORovereating"
+
+
+#calculate correlation between BAs and stress_outbreak
+#shopping
+require(plyr)
+funcshoppingb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORshopping = cor(plotcorrdisandBA$BAs_shopping, plotcorrdisandBA$stress_outbreak)))
+}
+bshopping <-ddply(plotcorrdisandBA, .(time_batch), funcshoppingb)
+
+#alcohol
+require(plyr)
+funcalcoholb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORalcohol = cor(plotcorrdisandBA$BAs_alcohol, plotcorrdisandBA$stress_outbreak)))
+}
+balcohol <-ddply(plotcorrdisandBA, .(time_batch), funcalcoholb)
+
+#smoking
+require(plyr)
+funcsmokingb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORsmoking = cor(plotcorrdisandBA$BAs_smoking, plotcorrdisandBA$stress_outbreak)))
+}
+bsmoking <-ddply(plotcorrdisandBA, .(time_batch), funcsmokingb)
+
+#legal drug
+require(plyr)
+funclegaldrugb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORlegaldrug = cor(plotcorrdisandBA$BAs_legal_drug, plotcorrdisandBA$stress_outbreak)))
+}
+blegaldrug <-ddply(plotcorrdisandBA, .(time_batch), funclegaldrugb)
+
+#illegal drug
+require(plyr)
+funcillegaldrugb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORillegaldrug = cor(plotcorrdisandBA$BAs_illegal_drug, plotcorrdisandBA$stress_outbreak)))
+}
+billegaldrug <-ddply(plotcorrdisandBA, .(time_batch), funcillegaldrugb)
+
+#gambling
+require(plyr)
+funcgamblingb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORgambling = cor(plotcorrdisandBA$BAs_gambling, plotcorrdisandBA$stress_outbreak)))
+}
+bgambling <-ddply(plotcorrdisandBA, .(time_batch), funcgamblingb)
+
+#gaming
+require(plyr)
+funcgamingb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORgaming = cor(plotcorrdisandBA$BAs_gaming, plotcorrdisandBA$stress_outbreak)))
+}
+bgaming <-ddply(plotcorrdisandBA, .(time_batch), funcgamingb)
+
+#overeating
+require(plyr)
+funcovereatingb <- function(plotcorrdisandBA)
+{
+  return(data.frame(CORovereating = cor(plotcorrdisandBA$BAs_overeating, plotcorrdisandBA$stress_outbreak)))
+}
+bovereating <-ddply(plotcorrdisandBA, .(time_batch), funcovereatingb)
+
+#make a dataframe for H1: Correlation BAs and stress_outbreak
+H1BAsstressoutbreak <- data.frame(bshopping, balcohol$CORalcohol, bsmoking$CORsmoking, blegaldrug$CORlegaldrug, billegaldrug$CORillegaldrug , bgambling$CORgambling, bgaming$CORgaming, bovereating$CORovereating)
+colnames(H1BAsstressoutbreak) #tidying the colnames
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "balcohol.CORalcohol"] <- "CORalcohol"
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "bsmoking.CORsmoking"] <- "CORsmoking"
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "blegaldrug.CORlegaldrug"] <- "CORlegaldrug"
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "billegaldrug.CORillegaldrug"] <- "CORillegaldrug"
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "bgambling.CORgambling"] <- "CORgambling"
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "bgaming.CORgaming"] <- "CORgaming"
+names(H1BAsstressoutbreak)[names(H1BAsstressoutbreak) == "bovereating.CORovereating"] <- "CORovereating"
+
 
 #######################################################
 ## H2: Self-reported addiction-related behavioral problems are related to general distress
